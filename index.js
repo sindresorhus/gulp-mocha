6699f8fb954ca0347be930698e03dfd61885e151
Sindre Sorhus <sindresorhus@gmail.com>
2014-01-09 13:56:35 +0100

    code style

--- index.js
+++ index.js
@@ -1,46 +1,42 @@
 'use strict';
-
 var path = require('path');
 var Duplex = require('stream').Duplex;
 var gutil = require('gulp-util');
 var Mocha = require('mocha');
 
 module.exports = function (options) {
+	var duplex = new Duplex({objectMode: true});
+	var errorCount = 0;
 
-    var duplex = new Duplex({ objectMode: true });
+	duplex._write = function (file, encoding, done) {
+		var mocha = new Mocha(options);
+		delete require.cache[require.resolve(path.resolve(file.path))];
+		mocha.addFile(file.path);
 
-    var errorCount = 0;
+		try {
+			mocha.run(function (errCount) {
+				duplex.push(file);
+				errorCount += errCount;
+				done();
+			}.bind(this));
+		} catch (err) {
+			this.emit('error', err);
+			done();
+		}
+	};
 
-    duplex._write = function _write(file, encoding, done) {
-        var mocha = new Mocha(options);
-        delete require.cache[require.resolve(path.resolve(file.path))];
-        mocha.addFile(file.path);
-        try {
-            mocha.run(function (errCount) {
-                duplex.push(file);
-                errorCount += errCount;
-                done();
-            }.bind(this));
-        } catch (err) {
-            this.emit('error', err);
-            done();
-        }
-    };
+	duplex.on('finish', function () {
+		if (errorCount === 0) {
+			return;
+		}
 
-    duplex.on('finish', function () {
-        if (errorCount === 0) { return; }
-        var ec = errorCount;
-        errorCount = 0;
-        duplex.emit('error',
-            new gutil.PluginError('gulp-mocha', [
-                ec,
-                (ec === 1 ? 'test' : 'tests'),
-                'failed.'
-            ].join(' '))
-        );
-    });
+		var ec = errorCount;
+		errorCount = 0;
 
-    duplex._read = function _read() {};
+		duplex.emit('error', new gutil.PluginError('gulp-mocha', [ec, (ec === 1 ? 'test' : 'tests'), 'failed.'].join(' ')));
+	});
 
-    return duplex;
+	duplex._read = function () {};
+
+	return duplex;
 };
