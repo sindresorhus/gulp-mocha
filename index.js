fbb549a3558a018dbdbe65572f3455170980be85
Robin Marr <robinemarr@gmail.com>
2014-04-19 23:43:13 +0100

    support mocha.opts file

--- index.js
+++ index.js
@@ -2,9 +2,32 @@
 var path = require('path');
 var gutil = require('gulp-util');
 var through2 = require('through2');
+var fs = require('fs');
+var util = require('util');
 var Mocha = require('mocha');
 
 module.exports = function (options) {
+
+	// mocha.opts support
+	try {
+		var multi = ['require'];
+		fs.readFileSync(path.resolve('test/mocha.opts'), 'utf8')
+			.trim()
+			.split(/\n/).map(function (opt) {
+				opt = opt.replace('--', '').split(/\s/)
+				var key = opt[0], value = opt[1];
+				if (!options.hasOwnProperty(key)) {
+					options[key] = value;
+				} else if ( util.isArray(options[key]) ) {
+					options[key].push(value);
+				} else if (~multi.indexOf(key)){
+					options[key] = [options[key], value];
+				}
+			});
+	} catch (err) {
+	// ignore
+	}
+
 	var mocha = new Mocha(options);
 	var cache = {};
 
