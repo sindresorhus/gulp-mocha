d14297abdcd14add5b89c865382f166624c63d24
Raphael Luba <raphael@leanbyte.com>
2014-07-08 18:33:52 +0200

    Close GH-43: Make sure that cache is cleared in every case..

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict';
-var path = require('path');
+
 var gutil = require('gulp-util');
 var through2 = require('through2');
 var Mocha = require('mocha');
+var domain = require('domain');
 
 module.exports = function (options) {
 	var mocha = new Mocha(options);
@@ -12,28 +13,44 @@ module.exports = function (options) {
 		cache[key] = true;
 	}
 
+	function clearCache() {
+		for (var key in require.cache) {
+			if (!cache[key]) {
+				delete require.cache[key];
+			}
+		}
+	}
+
 	return through2.obj(function (file, enc, cb) {
 		mocha.addFile(file.path);
 		this.push(file);
 		cb();
 	}, function (cb) {
-		try {
-			mocha.run(function (errCount) {
-				if (errCount > 0) {
-					this.emit('error', new gutil.PluginError('gulp-mocha', errCount + ' ' + (errCount === 1 ? 'test' : 'tests') + ' failed.'));
-				}
+		var stream = this;
+		var d = domain.create();
 
-				for (var key in require.cache) {
-					if (!cache[key]) {
-						delete require.cache[key];
-					}
-				}
-
-				cb();
-			}.bind(this));
-		} catch (err) {
-			this.emit('error', new gutil.PluginError('gulp-mocha', err));
+		function handleException(e) {
+			d.dispose();
+			clearCache();
+			stream.emit('error', new gutil.PluginError('gulp-mocha', e));
 			cb();
 		}
+
+		d.on('error', handleException);
+		d.run(function () {
+			try {
+				mocha.run(function (errCount) {
+					d.dispose();
+					clearCache();
+					if (errCount > 0) {
+						stream.emit('error', new gutil.PluginError('gulp-mocha', errCount + ' ' + (errCount === 1 ? 'test' : 'tests') + ' failed.'));
+					}
+					cb();
+				});
+			}
+			catch (e) {
+				handleException(e);
+			}
+		});
 	});
 };
