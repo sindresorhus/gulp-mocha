ec5984321138d501f79deb870f99b2fc75723196
Owen Evans <owen@bgeek.net>
2015-06-03 09:15:03 +1200

    fix syntax as suggested

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ module.exports = function (opts) {
 
 	function clearCache() {
 		for (var key in require.cache) {
-			if (!cache[key] && !key.match(/.*\.node$/)) {
+			if (!cache[key] && !/\.node$/.test(key)) {
 				delete require.cache[key];
 			}
 		}
