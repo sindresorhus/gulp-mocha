20bc2ced341f8d121c418f234a5e116ac7c87b0a
Sindre Sorhus <sindresorhus@gmail.com>
2015-07-12 14:01:21 +0200

    cleanup #102

--- test/require-test.js
+++ test/require-test.js
@@ -1,42 +1,49 @@
+'use strict';
 var path = require('path');
-var temp = require('temp');
-var mocha = require('../');
 var fs = require('fs');
 var assert = require('assert');
+var temp = require('temp');
+var mocha = require('../');
 
-var tempFile
-const filePrefix = './';
+var tempFile;
+var tempFileBaseName;
+var filePrefix = './';
 
 function removeFile(file) {
-  try {
-    fs.unlinkSync(file); 
-  } catch(err) {}
+	try {
+		fs.unlinkSync(file);
+	} catch (err) {}
 }
 
-beforeEach(function(){
-  tempFile = temp.path({dir:process.cwd(), suffix: '.js'});
-  tempFileBaseName = path.basename(tempFile);
-  fs.closeSync(fs.openSync(tempFile, 'w'));
+beforeEach(function () {
+	tempFile = temp.path({
+		dir: process.cwd(),
+		suffix: '.js'
+	});
+
+	tempFileBaseName = path.basename(tempFile);
+
+	fs.writeFileSync(tempFile, '');
 });
 
-afterEach(function(){
-  removeFile(tempFile);
+afterEach(function () {
+	removeFile(tempFile);
 });
 
+it('should fail when trying to require a file that doesn\'t exist', function () {
+	removeFile(tempFile);
 
-it('should fail when trying to require a file that doesn\'t exist', function() {
-  removeFile(tempFile);
-  assert.throws(function() {
-    mocha({ require: [filePrefix + tempFileBaseName]});
-  });
-})
-
-it('should be able to import js-files in cwd', function() {
-  mocha({ require: [filePrefix + tempFileBaseName]});
+	assert.throws(function () {
+		mocha({require: [filePrefix + tempFileBaseName]});
+	});
 });
 
-it('should fail when not having the ./ file prefix', function(){
-  assert.throws(function() {
-    mocha({require: [tempFileBaseName]}) 
-  });
+it('should be able to import js-files in cwd', function () {
+	mocha({require: [filePrefix + tempFileBaseName]});
+});
+
+it('should fail when not having the ./ file prefix', function () {
+	assert.throws(function () {
+		mocha({require: [tempFileBaseName]});
+	});
 });
