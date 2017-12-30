ac3d7fc8cfb13d98fa59ba5baef6a889abf5fa2b
Charles Samborski <demurgos@demurgos.net>
2017-12-30 22:27:16 +0100

    Drop dependency on deprecated `gulp-util` (#187)

--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
 import fs from 'fs';
 import path from 'path';
 import test from 'ava';
-import gutil from 'gulp-util';
+import Vinyl from 'vinyl';
 import pEvent from 'p-event';
 import mocha from '..';
 
 function fixture(name) {
 	const fileName = path.join(__dirname, 'fixtures', name);
 
-	return new gutil.File({
+	return new Vinyl({
 		path: fileName,
 		contents: fs.existsSync(fileName) ? fs.readFileSync(fileName) : null
 	});
@@ -33,9 +33,7 @@ test('pass async AssertionError to mocha', async t => {
 	const event = pEvent(stream, 'error');
 	stream.end(fixture('fixture-async.js'));
 	const error = await event;
-	const throws = /throws after timeout/.test(error.stdout);
-	const uncaught = /Uncaught AssertionError: false == true/.test(error.stdout);
-	t.true(throws || uncaught);
+	t.regex(error.stdout, /throws after timeout|Uncaught AssertionError.*: false == true/);
 });
 
 test('require two files', async t => {
