f69b13383f3710c4eadb8960153ee70a5362df44
Philip Whitehouse <pwhiteho@flextrade.com>
2023-10-26 13:54:12 +0100

    Try repointing self ref

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 'use strict';
 const gulp = require('gulp');
-const mocha = require('.');
+const mocha = require('index.js');
 
 exports.default = () => (
 	gulp.src('test/fixtures/fixture-pass.js', {read: false})
